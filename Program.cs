179886e1948010cf3d55c5a8fe200e41f57c5553
Janu shankar <janakiharishankar24@gmail.com>
2022-01-12 12:20:32 +0530

    added a file

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace EmployeeWagee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeePresence employeepresent = new EmployeePresence();
+            employeepresent.Employee_Check_UC1();
+            DailyEmployeeWage employee=new DailyEmployeeWage();
+            employee.Employee_Check_UC2();
+        }
+
+    }
+}
+
