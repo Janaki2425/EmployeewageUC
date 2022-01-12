5f821ce986078bdcc0155b6f23910d812223dde4
Janu shankar <janakiharishankar24@gmail.com>
2022-01-12 12:28:21 +0530

    added another file

---
+++ EmployeePresence.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWagee
+{
+    public class EmployeePresence
+    {
+            public void Employee_Check_UC1()
+            {
+                int IS_FULL_TIME = 1;
+                Random random = new Random();
+                int empcheck = random.Next(2);
+                Console.WriteLine(empcheck);
+                if (empcheck == IS_FULL_TIME)
+                {
+                    Console.WriteLine("Employee is present");
+                }
+                else
+                {
+                    Console.WriteLine("Employee is absent");
+                }
+            }
+        }
+    }
+
+
